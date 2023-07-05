acc3e332b5893cefca1dd72b81ca334aeb314009
Martinez, Oscar <Oscar.Martinez@sovos.com>
2023-07-05 14:47:15 -0700

    Added Strategy interface and extensions

---
+++ Business/Strategies/SalesTax/SwedenSalesTaxStrategy.cs
@@ -0,0 +1,50 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.SalesTax
+{
+    internal class SwedenSalesTaxStrategy : ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order)
+        {
+            var destination = order.ShippingDetails.DestinationCountry.ToLowerInvariant();
+            var origin = order.ShippingDetails.OriginCountry.ToLowerInvariant();
+             
+            if ( destination == origin)
+            {
+                return order.TotalPrice * 0.25m;
+            }
+              
+            #region Tax per item
+            //if (destination == ShippingDetails.OriginCountry.ToLowerInvariant())
+            //{
+            //    decimal totalTax = 0m;
+            //    foreach (var item in LineItems)
+            //    {
+            //        switch (item.Key.ItemType)
+            //        {
+            //            case ItemType.Food:
+            //                totalTax += (item.Key.Price * 0.06m) * item.Value;
+            //                break;
+
+            //            case ItemType.Literature:
+            //                totalTax += (item.Key.Price * 0.08m) * item.Value;
+            //                break;
+
+            //            case ItemType.Service:
+            //            case ItemType.Hardware:
+            //                totalTax += (item.Key.Price * 0.25m) * item.Value;
+            //                break;
+            //        }
+            //    }
+
+            //    return totalTax;
+            //}
+            #endregion
+
+            return 0;
+        }
+    }
+}
---
+++ Business/Strategies/SalesTax/USASalesTaxStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.SalesTax
+{
+    internal class USASalesTaxStrategy : ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order)
+        {
+            var destination = order.ShippingDetails.DestinationState.ToLowerInvariant();
+            switch (destination)
+            {
+                case "la": return order.TotalPrice * 0.095m;
+                case "ny": return order.TotalPrice * 0.04m;
+                case "nyc": return order.TotalPrice * 0.045m;
+                default: return 0m;
+            }
+        }
+    }
+}
